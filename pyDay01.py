f4aed414009c727598cf5b8be5777677a7c61be9
nueraihemaiti-k <nueraihemaiti-k@dmm.com>
2021-01-04 18:38:11 +0900

    day 1

---
+++ pyDay01.py
@@ -0,0 +1,24 @@
+x = int(input('please input a number:\n'))
+if x > 0:
+    print('its a plus number')
+elif x == 0:
+    pass
+else:
+    print('it is a minus number')
+a = int(input('please inter a:\n'))
+b = int(input('please inter b:\n'))
+if a == b:
+    print('a and b is equal')
+else:
+    print('a and b is not equal')
+print(False)
+print(True)
+y = int(input('please input y:\n'))
+if y > 0 and y <= 9:
+    print('gether then 0 and lessthen 10')
+elif y == 0:
+    print('y equal 0')
+elif y > 100:
+    print('gether then 100 ')
+else:
+    pass
